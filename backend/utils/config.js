441d6de408b98fea60961aa45cf15bae05bf94b2
Nikita Plokhikh <nika8123@ya.ru>
2023-10-05 16:19:20 +0300

    some fixes

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,6 @@
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 const URL_MONGO = 'mongodb://127.0.0.1:27017/mestodb';
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const { SECRET_KEY = 'some-secret-key' } = process.env;
 
 module.exports.URL_REGEX = URL_REGEX;
