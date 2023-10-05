e0ddd200718641e595250a53da31a15047f8c4bb
Nikita Plokhikh <nika8123@ya.ru>
2023-10-05 13:47:39 +0300

    15 sprint: some fixes

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,8 +1,9 @@
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 const URL_MONGO = 'mongodb://127.0.0.1:27017/mestodb';
 const { PORT = 3000 } = process.env;
+const { SECRET_KEY = 'some-secret-key' } = process.env;
 
 module.exports.URL_REGEX = URL_REGEX;
-module.exports.SECRET_KEY = process.env.JWT_SECRET;
+module.exports.SECRET_KEY = SECRET_KEY;
 module.exports.URL_MONGO = URL_MONGO;
 module.exports.PORT = PORT;
