5008fb7ca1e5cfc0b2d6cc313bb99e83586892af
Nikita Plokhikh <nika8123@ya.ru>
2023-10-06 11:40:53 +0300

    Исрпвления после первого ревью

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { rateLimit } = require('express-rate-limit');
 const { URL_MONGO, PORT } = require('./utils/config');
 const router = require('./routes');
 const cors = require('./middlewares/cors');
+const NotFound = require('./utils/errors/NotFound');
+const { ERROR_INTERNAL_SERVER } = require('./utils/http_codes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+router.use(requestLogger);
 
 const app = express();
 
@@ -12,11 +20,35 @@ mongoose.connect(URL_MONGO, {
   useNewUrlParser: true,
 });
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors);
 
 app.use(router);
+
+app.use('*', (req, res, next) => {
+  next(new NotFound('Такой страницы не существует'));
+});
+
+app.use(errorLogger);
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = ERROR_INTERNAL_SERVER, message } = err; // 500 by default
+  res.status(statusCode).send({ message });
+
+  next();
+});
 
 app.listen(PORT);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,10 @@
 const router = require('express').Router();
-const { errors } = require('celebrate');
-
 const auth = require('../middlewares/auth');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const routeSignup = require('./auth/signup');
 const routeSignin = require('./auth/signin');
-const NotFound = require('../utils/errors/NotFound');
-const { ERROR_INTERNAL_SERVER } = require('../utils/http_codes');
-const { requestLogger, errorLogger } = require('../middlewares/logger');
 
-router.use(requestLogger);
 router.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
@@ -25,19 +19,5 @@ router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-
-router.use('*', (req, res, next) => {
-  next(new NotFound('Такой страницы не существует'));
-});
-
-router.use(errorLogger);
-router.use(errors());
-
-router.use((err, req, res, next) => {
-  const { statusCode = ERROR_INTERNAL_SERVER, message } = err; // 500 by default
-  res.status(statusCode).send({ message });
-
-  next();
-});
 
 module.exports = router;
